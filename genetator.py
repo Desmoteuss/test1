b623ea7ff140b5c32923f0360727986e3ef0a0b7
desmoteuss <desmoteuss@gmail.com>
2021-03-23 22:07:00 +0100

    initial commit

---
+++ genetator.py
@@ -0,0 +1,14 @@
+import sys
+password = []
+character_left = -1
+
+password_lenght = int(input("how long?"))
+
+if password_lenght < 5:
+    print("Min 5 character, please reenter")
+    sys.exit(0)
+
+else:
+
+character_left = password_lenght
+
